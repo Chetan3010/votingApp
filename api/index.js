6bee235ed2f13a530bc3178dab12c96cde2ba838
chetan.3010 <ckale2001@gmail.com>
2024-09-25 21:40:23 +0530

    ⚒ updated files

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const db = require('../db')
+const db = require('./db')
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
-const userRoutes = require('../routes/userRoutes')
-const candidateRoutes = require('../routes/candidateRoutes')
+const userRoutes = require('./routes/userRoutes')
+const candidateRoutes = require('./routes/candidateRoutes')
 
 app.get('/', (req, res) => {
     res.send(`
@@ -30,4 +30,4 @@ app.get('/', (req, res) => {
 app.use('/', userRoutes)
 app.use('/candidate', candidateRoutes)
 
-app.listen(3000, () => console.log("Listening on port 3000..."))
+app.listen(3005, () => console.log("Listening on port 3005..."))
